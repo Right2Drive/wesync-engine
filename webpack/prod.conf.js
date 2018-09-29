a08cb8630c1a514b63227436a3f95a5c2f4cbb77
Parasrah <parasrah@tutanota.com>
2018-09-29 02:35:53 -0600

    Removing webpack

--- webpack/prod.conf.js
+++
@@ -1,8 +0,0 @@
-const merge = require('webpack-merge')
-
-const baseConfig = require('./base.conf.js')
-
-module.exports = () => merge(baseConfig(), {
-  mode: 'production',
-  devtool: 'source-map',
-})
--- webpack/utils.js
+++
@@ -1,3 +0,0 @@
-const path = require('path')
-
-exports.root = (...paths) => path.resolve(__dirname, '..', ...paths)
